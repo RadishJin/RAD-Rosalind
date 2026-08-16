11d7c336deac7b9d5a131e9d22f2ed22a77f694a
RadishJin <radishj24@gmail.com>
2026-08-16 11:21:56 +0900

    Solved DNA & Comments

--- Bioinformatics_Stronghold/Rosalind_DNA.py
+++ Bioinformatics_Stronghold/Rosalind_DNA.py
@@ -6,10 +6,17 @@ with open("data/rosalind_dna.txt", "r") as f:
 sequence = raw_data.strip()
 
 Bases = Counter(sequence)
+Sorted_Bases = dict(sorted(Bases.items()))
 
-for N in Bases.values():
+# print(f"{Sorted_Bases})")
+
+for N in Sorted_Bases.values():
     print(N, end=" ")
 
 # 딕셔너리.values() : 딕셔너리의 value만 반환
 # 딕셔너리.items() : 딕셔너리의 key, value를 튜플 형태로 반환
 # 딕셔너리.keys() : 딕셔너리의 key만 반환
+
+# sorted(딕셔너리) : 딕셔너리의 key를 기준으로 정렬된 리스트 반환 (Value 데이터 다 날아감)
+# sorted(딕셔너리.items()) : 딕셔너리의 key를 기준으로 정렬된 튜플 리스트 반환
+# dict(튜플 리스트) : 튜플 리스트를 딕셔너리로 반환
--- Python_Village/Rosalind_INI5.py
+++ Python_Village/Rosalind_INI5.py
@@ -11,3 +11,6 @@ for i in range(len(raw_data)):
 
 for j in str_list:
     print(f"{j}")
+
+# .readlines() : 파일 전체를 읽어옴, 각 줄을 요소로 갖는 리스트로 반환
+# .readline() : 파일 전체를 읽어옴, 한 줄씩 읽어옴, 한 줄을 string으로 반환
