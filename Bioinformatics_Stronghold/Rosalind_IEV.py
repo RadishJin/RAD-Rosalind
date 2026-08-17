3788c518d5cb913d984f8b96b55e187842bbdf52
RadishJin <radishj24@gmail.com>
2026-08-17 16:34:03 +0900

    Solved IEV

---
+++ Bioinformatics_Stronghold/Rosalind_IEV.py
@@ -0,0 +1,14 @@
+with open("data/rosalind_iev.txt", "r") as f:
+    nums = list(map(int, f.read().strip().split()))
+
+# raw = "1 0 0 1 0 1"
+
+# nums = list(map(int, raw.strip().split()))
+# print(nums)
+
+def Offspring(n: list[int]) -> float:
+    Dominant = [1.0, 1.0, 1.0, 0.75, 0.5, 0.0]
+    dot_product = sum(2 * x * y for x, y in zip(n, Dominant))
+    return dot_product
+
+print(f"{Offspring(nums):.1f}")
